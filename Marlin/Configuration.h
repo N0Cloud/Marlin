778b274e6ec8eb8ebb62e3eb759d605c7b030df1
Gabriel Poulenard <N0Cloud@users.noreply.github.com>
2020-07-26 14:13:01 -0400

    adjust MIN_PROBE_EDGE and Z offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -966,11 +966,11 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 23, 5, -0.925 }
+#define NOZZLE_TO_PROBE_OFFSET { 23, 5, -0.850 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define MIN_PROBE_EDGE 20
+#define MIN_PROBE_EDGE 10
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
